eaa314863e61260e463a4c82f2414f4ac1b7e3da
ReMinoer <reminoer@gmail.com>
2021-06-17 22:34:19 +0200

    Fix hour format in default file & folder names

--- PostMortem/Utils/CrashContextExtension.cs
+++ PostMortem/Utils/CrashContextExtension.cs
@@ -4,12 +4,12 @@
     {
         static public string GetDefaultFileName(this ICrashContext crashContext, string prefix, string extension, string id = null)
         {
-            return $"{prefix}{id}_{crashContext.SourceName}_{crashContext.Timestamp:yyyy-MM-dd_hh-mm-ss}.{extension}";
+            return $"{prefix}{id}_{crashContext.SourceName}_{crashContext.Timestamp:yyyy-MM-dd_HH-mm-ss}.{extension}";
         }
 
         static public string GetDefaultFolderName(this ICrashContext crashContext, string prefix)
         {
-            return $"{prefix}_{crashContext.SourceName}_{crashContext.Timestamp:yyyy-MM-dd_hh-mm-ss}";
+            return $"{prefix}_{crashContext.SourceName}_{crashContext.Timestamp:yyyy-MM-dd_HH-mm-ss}";
         }
     }
 }
